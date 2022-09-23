461e6f5084c02f4b1d2cbc16bab0d4392baa9625
Gokberk Alagoz <gokala@lux13.mpi.nl>
2022-09-23 15:48:49 +0200

    update

--- ldsc/run_partherit_fetalHGE.sh
+++ ldsc/run_partherit_fetalHGE.sh
@@ -3,4 +3,4 @@
 #$ -q multi15.q
 #$ -S /bin/bash
 
-/data/workspaces/lag/workspaces/lg-genlang/Working/SCRIPTS/23andMe/Dyslexia/dyslexiaevol/clap-beat_dyslexia/ldsc/partherit_baseline_and_extraAnnot.sh /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/data/munged/flippedZRhythym.sumstats.gz fetal_hge_hg19.merged.sorted /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/partherit/flippedZRhythym.fetalHGE.results E081_active_marks
+/data/workspaces/lag/workspaces/lg-genlang/Working/SCRIPTS/23andMe/clap-beat_dyslexia/pleiotropyevo/ldsc/partherit_baseline_and_extraAnnot.sh /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/gSEM/GenomicSEM_multivarGWAS_dys_rhyimp_v2.sumstats.gz fetal_hge_hg19.merged.sorted /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/partherit/GenomicSEM_multivarGWAS_dys_rhyimp_v2.fetalHGE.results E081_active_marks
--- ldsc/run_partherit_neanDep.sh
+++ ldsc/run_partherit_neanDep.sh
@@ -3,4 +3,4 @@
 #$ -q multi15.q
 #$ -S /bin/bash
 
-/data/workspaces/lag/workspaces/lg-genlang/Working/SCRIPTS/23andMe/Dyslexia/dyslexiaevol/clap-beat_dyslexia/ldsc/partherit_baseline.sh /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/data/munged/flippedZRhythym.sumstats.gz neanDepRegions_hg19.sorted /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/partherit/flippedZRhythym.neanDepRegions.results
+/data/workspaces/lag/workspaces/lg-genlang/Working/SCRIPTS/23andMe/clap-beat_dyslexia/pleiotropyevo/ldsc/partherit_baseline.sh /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/gSEM/GenomicSEM_multivarGWAS_dys_rhyimp_v2.sumstats.gz neanDepRegions_hg19.sorted /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/partherit/GenomicSEM_multivarGWAS_dys_rhyimp_v2.neanDepRegions.results
