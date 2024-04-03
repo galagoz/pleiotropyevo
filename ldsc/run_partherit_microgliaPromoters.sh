db78a31c00778eee2af8d0f3e8f74a363fbc24f4
galagoz <gokberk.alagoz@gmail.com>
2024-04-03 10:44:17 +0200

    update

---
+++ ldsc/run_partherit_microgliaPromoters.sh
@@ -0,0 +1,7 @@
+#$ -N partherit
+#$ -cwd
+#$ -q multi15.q
+#$ -S /bin/bash
+
+# Genomic SEM common factor results
+/data/workspaces/lag/workspaces/lg-genlang/Working/SCRIPTS/23andMe/clap-beat_dyslexia/pleiotropyevo/ldsc/partherit_baseline.sh /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/data/munged/GenomicSEM_multivarGWAS_CPM_dys_rhyimp.sumstats.gz microglia_promoters.sorted /data/clusterfs/lag/users/gokala/beat-dyslexiaevol/results/partherit/GenomicSEM_multivarGWAS_dys_rhyimp.microgliaPromoters
